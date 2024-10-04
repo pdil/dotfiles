73c3d5c06cd4456a51e689afcdf090e177068b17
Paolo Di Lorenzo <dilorenzo@hey.com>
2024-10-03 22:42:52 -0400

    Enable slow motion minimize animation in defaults.sh

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -4,4 +4,7 @@
 defaults write com.apple.dt.Xcode ShowBuildOperationDuration YES
 
 # allow Cmd+Ctrl+Click drag gesture anywhere on windows
-defaults write -g NSWindowShouldDragOnGesture -bool true
+defaults write -g NSWindowShouldDragOnGesture -bool YES
+
+# allow Shift+Click minimize button for slow motion animation
+defaults write com.apple.dock slow-motion-allowed -bool YES
